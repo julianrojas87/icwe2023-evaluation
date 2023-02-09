32444689f8f60667994a2bf253400fec6b85b58c
Julian Rojas <julian.rojas87@gmail.com>
2023-02-09 01:29:52 +0100

    Adjust measurement hook

--- measurement-hook/index.js
+++ measurement-hook/index.js
@@ -6,21 +6,22 @@ const { exec } = require('child_process');
 http.createServer((req, res) => {
     const urlObj = new URL(req.url, `http://${req.headers.host}`);
     const command = urlObj.searchParams.get('command');
+    const test = urlObj.searchParams.get('test');
     const module = urlObj.searchParams.get('module');
     const server = urlObj.searchParams.get('server');
-    const concurrency = urlObj.searchParams.get('concurrency');
+    const clients = urlObj.searchParams.get('clients');
 
     if (command === 'start') {
         if(module === 'gdb') {
-            if(!fs.existsSync(`../graph-database/${server}/results`)) {
-                fs.mkdirSync(`../graph-database/${server}/results`);
+            if(!fs.existsSync(`../graph-database/${server}/${test}/results`)) {
+                fs.mkdirSync(`../graph-database/${server}/${test}/results`);
             }
-            exec(`sudo ./recordstats.sh > ../graph-database/${server}/results/${concurrency}.csv`);
+            exec(`sudo ./recordstats.sh > ../graph-database/${server}/results/${test}/${clients}.csv`);
         } else {
-            if(!fs.existsSync(`../tiles-interface/results`)) {
-                fs.mkdirSync(`../tiles-interface/results`);
+            if(!fs.existsSync(`../tiles-interface/results/${test}`)) {
+                fs.mkdirSync(`../tiles-interface/results/${test}`);
             }
-            exec(`sudo ./recordstats.sh > ../tiles-interface/results/${concurrency}.csv`);
+            exec(`sudo ./recordstats.sh > ../tiles-interface/results/${test}/${clients}.csv`);
         }
     }
 
