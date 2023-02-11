907f77f8b1b1517332acc3560d0621b1655934fb
Julian Rojas <julian.rojas87@gmail.com>
2023-02-11 01:14:46 +0100

    Set root as user for stardog container

--- graph-database/stardog/start-stardog.sh
+++ graph-database/stardog/start-stardog.sh
@@ -11,4 +11,4 @@ fi
 
 # Start Stardog Docker container
 sudo docker pull stardog/stardog:latest
-sudo docker run -it -v `pwd`/home:/var/opt/stardog -v `pwd`/data:/home --name stardog -p 5820:5820 -e STARDOG_SERVER_JAVA_ARGS="-Xmx8g -Xms8g -XX:MaxDirectMemorySize=12g" -d stardog/stardog
+sudo docker run -it -v `pwd`/home:/var/opt/stardog -v `pwd`/data:/home --name stardog --user root -p 5820:5820 -e STARDOG_SERVER_JAVA_ARGS="-Xmx8g -Xms8g -XX:MaxDirectMemorySize=12g" -d stardog/stardog
